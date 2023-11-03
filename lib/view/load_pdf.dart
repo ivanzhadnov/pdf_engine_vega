1b456d7890485d52a63504828d8092d630b08cdb
Дмитрий Колдашев <dmitry@koldashev.ru>
2023-11-03 14:14:06 +0200

    пофиксил под андроид

--- lib/view/load_pdf.dart
+++ lib/view/load_pdf.dart
@@ -56,7 +56,7 @@ class LoadPdf{
     catch (e) {
       debugPrint('Load UserAgreement from Assets error: $e');
     }
-    if(Platform.isAndroid)await _pdfDocument.close();
+    await _pdfDocument.close();
     return bytes;
   }
 
